9bef6e0d22808fc5bd520a13e4c1711b4868b7e4
stanNthe5 <st@jcow.net>
2025-06-10 07:55:03 +0800

    fix: wrong wallet Icon size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,8 +6,8 @@ export default function Footer() {
     const { publicKey } = useWallet()
     return <div className="footer">
         <div style={{ display: 'flex', alignItems: 'center', gap: 10, justifyContent: 'center' }}>
-            <a href="https://x.com/theprivacycash"><IconBtn name="x" /></a>
-            <a href="https://github.com/Privacy-Cash/privacy-cash"><IconBtn name="github" /></a>
+            <a href="https://x.com/theprivacycash" target="_blank"><IconBtn name="x" /></a>
+            <a href="https://github.com/Privacy-Cash/privacy-cash" target="_blank"><IconBtn name="github" /></a>
         </div>
     </div>
 }
